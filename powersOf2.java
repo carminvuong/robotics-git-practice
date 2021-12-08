b38b250e78ef4f87791b790c76dc7c6fe1302543
Carmin Vuong <carminvuong@gmail.com>
2021-12-08 17:38:59 -0500

    add file

---
+++ powersOf2.java
@@ -0,0 +1,13 @@
+public class powersOf2 {
+    public static void main(String[] args) {
+        int target = 694;
+        int num = 1;
+        while (num <= target) {
+            
+            System.out.println(num);
+            num = num * 2;
+        }
+    }
+}
+// aiosnioanoasnoiansdioandoiadoiaoisndaijdnfsndfipnsPIDFNipsdnfipsndfisnadifbnsaijdpfnsijdnfijpsdnfipsjdnfsaiupn
+// comment commet c0emtn comemmt comemt comemt
